ce2e1bff2c2c4b5013f09444d113cd7b21c67314
DISC0MF0RT <pantyushinkm@gmail.com>
2025-03-16 15:45:26 -0400

    n2 + n1/dopolnenie

--- src/matrix.h
+++ src/matrix.h
@@ -22,7 +22,17 @@ public:
 
     void printElement(int r, int c);
 
+    bool isTriDiagonal();
+
+    double determinant() const;
+
+    std::vector<std::vector<double>> inverse() const;
+
     std::vector<double> gaussJordanSolve(std::vector<double> b);
+
+    std::vector<double> progon(std::vector<double> b);
+
+    bool check(std::vector<double> x, std::vector<double> b);
 };
 
 #endif
